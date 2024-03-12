cd88463187fdf218ba017ca4c1eb6079d1397163
Yeeloman <yami.onlyme@gmail.com>
2024-03-12 16:43:16 +0000

    add task 1 of 0x01. Lockboxes
    a method that determines if all the boxes can be opened.
    
    Prototype: def canUnlockAll(boxes)
    boxes is a list of lists
    A key with the same number as a box opens that box
    You can assume all keys will be positive integers
    There can be keys that do not have boxes
    The first box boxes[0] is unlocked
    Return True if all boxes can be opened, else return False

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" function that determines
+if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """ function that determines if all the boxes can be opened"""
+    unlocked = [0] * len(boxes)
+    unlocked[0] = 1
+
+    for box in boxes[0]:
+        if box < len(boxes):
+            unlocked[box] = 1
+
+    for i in range(len(boxes)):
+        for j in range(len(boxes)):
+            if unlocked[j] == 1:
+                for box in boxes[j]:
+                    if box < len(boxes):
+                        unlocked[box] = 1
+
+    if 0 in unlocked:
+        return False
+    else:
+        return True
