34aa8fb78e03576f2dde9465f280a96884974bd6
Yeeloman <yami.onlyme@gmail.com>
2024-04-11 12:33:22 +0000

    add the n queen problem

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""0. N queens
+alx interview task"""
+
+
+import sys
+
+
+def isNvalid():
+    """checks if N is valid"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+
+        solveNqueen(n)
+    except TypeError:
+        print('N must be a number')
+        sys.exit(1)
+
+
+def solveNqueen(n: int):
+    """main function to solve the N queen problem"""
+    col = set()
+    posDiag = set()  # r+c
+    negDiag = set()  # r-c
+    resp = []
+
+    def backtrack(r, solution):
+        """implementation for backtrack algo"""
+        if r == n:
+            resp.append(solution)
+            return
+
+        for c in range(n):
+            if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                continue
+
+            col.add(c)
+            posDiag.add(r+c)
+            negDiag.add(r-c)
+
+            backtrack(r + 1, solution + [[r, c]])
+
+            col.remove(c)
+            posDiag.remove(r+c)
+            negDiag.remove(r-c)
+
+    backtrack(0, [])
+    print(resp)
+
+
+if __name__ == "__main__":
+    isNvalid()
