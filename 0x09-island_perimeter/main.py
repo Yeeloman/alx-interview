571edd9d6aba5fea64001235c7d112ed27b6aba4
Yeeloman <yami.onlyme@gmail.com>
2024-05-09 13:05:57 +0100

    island perimiter

--- 0x09-island_perimeter/main.py
+++ 0x09-island_perimeter/main.py
@@ -3,6 +3,7 @@
 0-main
 """
 island_perimeter = __import__('0-island_perimeter').island_perimeter
+island_perimeter2 = __import__('test').island_perimeter2
 
 if __name__ == "__main__":
     grid = [
@@ -12,4 +13,20 @@ if __name__ == "__main__":
         [0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0]
     ]
+
+    grid2 = [
+        [0,0,0,0,0,0],
+        [0,1,1,1,0,0],
+        [0,1,1,1,0,0],
+        [0,1,1,1,0,0]
+    ]
+
+    grid3 = [[0], [0], [0]]
+
     print(island_perimeter(grid))
+    print(island_perimeter(grid2))
+    print(island_perimeter(grid3))
+    print('########################')
+    print(island_perimeter2(grid))
+    print(island_perimeter2(grid2))
+    print(island_perimeter2(grid3))
