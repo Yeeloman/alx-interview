46bc5d5de0bc00a99c3a6c0397213524536c020f
Yeeloman <yami.onlyme@gmail.com>
2024-05-15 15:11:50 +0100

    lets see

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,64 +20,41 @@ def isWinner(x, num):
                 return False
         return True
 
-    def removeMultiples(arr, n):
+    def makeArr(num):
+        """i do not make"""
+        return list(range(1, num + 1))
+
+    def removeMult(arr, n):
         """ banished from the heavens"""
         arr[:] = [x for x in arr if x % n != 0]
 
     # how many rounds are there
-    for n in num:
-        mariaTurn = True
-        round_array = list(range(1, n + 1))
-
-        while round_array:
-            prime_found = False
-            for num in round_array:
-                if isPrime(num):
-                    prime_found = True
-                    round_array = removeMultiples(round_array, num)
-                    mariaTurn = not mariaTurn
-                    break  # Move to the next round after finding a prime
-            if not prime_found:
-                break  # No prime found, the other player wins this round
-
+    for i in range(x):
+        thisRoundArr = makeArr(num[i])
+        # loops for each round's array
+        for thisNum in thisRoundArr:
+            if mariaTurn:
+                if isPrime(thisNum):
+                    # thisRoundArr.remove(thisNum)
+                    removeMult(thisRoundArr, thisNum)
+                    mariaTurn = False
+                else:
+                    continue
+            else:
+                if isPrime(thisNum):
+                    # thisRoundArr.remove(thisNum)
+                    removeMult(thisRoundArr, thisNum)
+                    mariaTurn = True
+                else:
+                    continue
         if mariaTurn:
             mariaWins += 1
         else:
             benWins += 1
 
-    if benWins == mariaWins:
-        return None
-    elif benWins > mariaWins:
-        return "Ben"
-    else:
-        return "Maria"
-    #
-    # for i in range(x):
-    #     thisRoundArr = makeArr(num[i])
-    #     # loops for each round's array
-    #     for thisNum in thisRoundArr:
-    #         if mariaTurn:
-    #             if isPrime(thisNum):
-    #                 # thisRoundArr.remove(thisNum)
-    #                 removeMult(thisRoundArr, thisNum)
-    #                 mariaTurn = False
-    #             else:
-    #                 continue
-    #         else:
-    #             if isPrime(thisNum):
-    #                 # thisRoundArr.remove(thisNum)
-    #                 removeMult(thisRoundArr, thisNum)
-    #                 mariaTurn = True
-    #             else:
-    #                 continue
-    #     if mariaTurn:
-    #         mariaWins += 1
-    #     else:
-    #         benWins += 1
-    #
-    #     if benWins == mariaWins:
-    #         return None
-    #     elif benWins > mariaWins:
-    #         return "Ben"
-    #     else:
-    #         return "Maria"
+        if benWins == mariaWins:
+            return None
+        elif benWins > mariaWins:
+            return "Ben"
+        else:
+            return "Maria"
